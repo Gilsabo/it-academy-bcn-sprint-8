1c5fbbcf86ed5a1a5b25f7d21be23c9d02f68646
Gil Sala <gilsala_88@hotmail.com>
2023-05-18 10:38:22 +0200

    Add carpet and image to project

--- src/Layouts/Layouts.js
+++ src/Layouts/Layouts.js
@@ -1,11 +1,12 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "./layouts.css"
+import logoStarWars from "../../images/logo-starwars.png"
 
 const RootLayouts = () => {
   return (
     <div className="root-layout">
       <header className="App-header">
-        <h1>Star wars</h1>
+        <img src={logoStarWars} alt="logo-star-wars" />
         <div className="links">
           <NavLink to="/home">
             <h3 className="home-nav">HOME</h3>
