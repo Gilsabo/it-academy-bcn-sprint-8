dcca38a0a637bddd8bf1f97f363c9d344011df22
Gil Sala <gilsala_88@hotmail.com>
2023-05-19 09:13:01 +0200

    Add styles

--- src/Layouts/Layouts.js
+++ src/Layouts/Layouts.js
@@ -1,18 +1,18 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "./layouts.css"
-import logoStarWars from "../../images/logo-starwars.png"
+import logoStarWars from "../images/logo-starwars.png"
 
 const RootLayouts = () => {
   return (
     <div className="root-layout">
       <header className="App-header">
-        <img src={logoStarWars} alt="logo-star-wars" />
+        <img className="logo-star-wars" src={logoStarWars} alt="logo-star-wars" />
         <div className="links">
-          <NavLink to="/home">
-            <h3 className="home-nav">HOME</h3>
+          <NavLink className="home-nav"to="/home">
+            <h3 >HOME</h3>
           </NavLink>
-          <NavLink to="/starships">
-            <h3 className="starship-nav">STARSHIPS</h3>
+          <NavLink className="starship-nav" to="/starships">
+            <h3 >STARSHIPS</h3>
           </NavLink>
         </div>
       </header>
