629274308d4478389f392956e7211ade43926594
Gil Sala <gilsala_88@hotmail.com>
2023-05-21 09:05:42 +0200

    Add funcionality to load more data by clicking the button

--- src/Components/Starships/Starships.js
+++ src/Components/Starships/Starships.js
@@ -7,15 +7,18 @@ import { Link } from "react-router-dom";
 const Starships = () => {
   const [starShipsData, setStarShipsData] = useState(null);
   const [numberUrl, setNumberURl] = useState(null);
+  
 
   useEffect(() => {
     axios
       .get("https://swapi.py4e.com/api/starships/?page=1")
       .then((response) => {
         setStarShipsData(response.data.results);
+        
         const numbers = response.data.results.map((item) => {
           const regex = /\/(\d+)\//;
           const number = item.url.match(regex);
+          
           return parseInt(number[1]);
         });
         setNumberURl(numbers);
@@ -26,7 +29,39 @@ const Starships = () => {
   }, []);
 
   console.log(numberUrl);
+  
+  const [addOnePage, setAddOnePage] =useState(2)
 
+  
+
+   const loadMoreData=()=>{    
+    const one= 1
+      setAddOnePage((prevPageNumber => prevPageNumber + one));
+      console.log(addOnePage)
+   
+    
+    axios
+      .get(`https://swapi.py4e.com/api/starships/?page=${addOnePage}`)
+      .then((response) => {
+        
+        console.log(response.data.next)
+        console.log(response)
+        setStarShipsData(preStarShipsData=>([...preStarShipsData,...response.data.results]))
+        const numbers = response.data.results.map((item) => {
+          const regex = /\/(\d+)\//;
+          const number = item.url.match(regex);
+          
+          return parseInt(number[1]);
+        });
+        setNumberURl(prevNumbers=>[...prevNumbers, ...numbers]);
+        console.log(numberUrl)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+ 
+  
   return (
     <>
       {starShipsData && (
@@ -43,7 +78,7 @@ const Starships = () => {
               </div>
             </Link>
           ))}
-          <button type="button">View more...</button>
+          <button onClick={loadMoreData} type="button">View more...</button>
         </div>
       )}
     </>
