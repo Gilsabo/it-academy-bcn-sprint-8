43c8e4997d9f6706a2d984ff32a6c04fc90aa550
Gil Sala <gilsala_88@hotmail.com>
2023-05-30 09:35:52 +0200

    Add components Pilots and films and conditional rendering depending on image found or not

--- src/Components/StarshipDetail/StarshipDetail.js
+++ src/Components/StarshipDetail/StarshipDetail.js
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLoaderData } from "react-router-dom";
 import "./starshipDetail.css";
+import Pilots from "../Pilots/Pilots";
+import Films from "../Films/Films";
+import forofor from "../../images/darth-vader.jpg";
 
 const StarshipDetail = () => {
   const { numberUrl } = useParams();
   const starship = useLoaderData();
   const [imageStarship, setImageStarship] = useState(null);
-  console.log(starship);
+  const [imageError, setImageError] = useState(false);
 
   useEffect(() => {
     setImageStarship(
@@ -15,30 +17,30 @@ const StarshipDetail = () => {
         numberUrl +
         ".jpg"
     );
-  }, []);
+  }, [numberUrl]);
+
+  const handleImageError = () => {
+    setImageError(true);
+  };
 
   return (
     <>
-      {imageStarship && <img className="image-starship" src={imageStarship} alt="fetched-starship" />}
-      
+      {imageStarship && !imageError ? (
+        <img
+          className="image-starship"
+          src={imageStarship}
+          alt="fetched-starship"
+          onError={handleImageError}
+        />
+      ) : (
+        <img className="image-starship" src={forofor} alt="fetched-starship" />
+      )}
+
       <div className="starship">
-        <div className="container-left">
-          <header className="name-starship"><h1> {starship.name}</h1></header>
-          <p>Model: {starship.model} </p>
-          <p>Cost in credits: {starship.cost_in_credits}</p>
-          <p>Atmospheric speed: {starship.max_atmosphering_speed}</p>
-          <p>MGLT: {starship.MGLT}</p>
-          <p>Consumables: {starship.consumables}</p>
-          <p>Passengers: {starship.passengers}</p>
-        </div>
-        <div className="container-right">
-          <p>Manufacturer: {starship.manufacturer}</p>
-          <p>Length: {starship.length}</p>
-          <p>Crew: {starship.crew}</p>
-          <p>Cargo Capacity: {starship.cargo_capacity}</p>
-          <p>Manufacturer: {starship.manufacturer}</p>
-        </div>
+        {/* Rest of the component */}
       </div>
+      <Pilots starship={starship} />
+      <Films starship={starship} />
     </>
   );
 };
@@ -46,11 +48,7 @@ const StarshipDetail = () => {
 export default StarshipDetail;
 
 export const starshipDetailsLoader = async ({ params }) => {
-  console.log(params);
-
   const { numberUrl } = params;
-
   const res = await fetch("https://swapi.py4e.com/api/starships/" + numberUrl);
-
   return res.json();
 };
