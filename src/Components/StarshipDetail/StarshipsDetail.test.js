5490bd7d87dbe08877253c55977fe11faea3a8ab
Gil Sala <gilsala_88@hotmail.com>
2023-06-01 08:13:53 +0200

    Add lost starhispdetial?

---
+++ src/Components/StarshipDetail/StarshipsDetail.test.js
@@ -0,0 +1,8 @@
+/*import React from "react";
+import "@testing-library/jest-dom/extend-expect"
+import {render} from "@testing-library/react"
+import StarshipsDetail from "./StarshipsDetail"
+
+test (" renders content", () =>{
+    const 
+})*/
