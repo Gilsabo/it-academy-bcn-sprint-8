be0709a30f146aedd4e6db1fc2563cac92875624
Gil Sala <gilsala_88@hotmail.com>
2023-06-01 10:43:26 +0200

    Add test Films

--- src/Components/Films/Films.test.js
+++ src/Components/Films/Films.test.js
@@ -1,9 +1,6 @@
 import React from "react";
-
 import {screen, render} from "@testing-library/react"
 import Films from "./Films"
-
-
 
 describe('Films', () => {
   it('should render the Films component with the title', () => {
