5381a135b74be5232dcee2e7f487471b9bb6c934
Gil Sala <gilsala_88@hotmail.com>
2023-06-02 10:04:22 +0200

    Go back to createBrowserRouter and trying out basename

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Starships from "./Components/Starships/Starships";
 import {
-  BrowserRouter as Router,
-  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
   Route,
+  RouterProvider,
+
 } from "react-router-dom";
 import Login from "./Components/login/Login";
 import Home from "./Components/Home/Home";
@@ -12,20 +14,23 @@ import StarshipDetail, { starshipDetailsLoader } from "./Components/StarshipDeta
 import { useState } from "react";
 
 function App() {
-  const [userData, setUserData] = useState(null);
-
+  const [userData, setUserData] = useState(null);  
+ 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route exact  path="/" element ={<RootLayouts userData={userData}/> }>
+        <Route exact path="home" element={<Home />} />
+        <Route exact path="starships" element={<Starships />} />
+        <Route exact path="starships/:numberUrl" element= {< StarshipDetail/>} loader={starshipDetailsLoader} />
+        <Route exact path="login" element= {<Login  userData={userData} setUserData={setUserData}/> }  />
+      </Route>
+    ),
+    { basename: process.env.PUBLIC_URL}
+  );
+console.log(router)
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path={process.env.PUBLIC_URL + "/"} element={<RootLayouts userData={userData} />} >
-            <Route path="home" element={<Home />} />
-            <Route path="starships" element={<Starships />} />
-            <Route path="starships/:numberUrl" element={<StarshipDetail />} loader={starshipDetailsLoader} />
-            <Route path="login" element={<Login userData={userData} setUserData={setUserData} />} />
-          </Route>
-        </Routes>
-      </Router>
+      <RouterProvider router={router } />
     </div>
   );
 }
